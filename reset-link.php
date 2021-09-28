1928d7455faae96def385a61347b17ca99a7aede
Prasun Neupane <prasunneuane14@gmail.com>
2021-09-28 16:23:54 +0545

    Bootstrap added on jquery

--- reset-link.php
+++ reset-link.php
@@ -1,15 +1,27 @@
 <html>
+  <head>
+    <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  </head>
   <body>
     <form method="post" action="password-reset-token.php">
       <p>Enter Email Address To Send Password Link</p>
       <div class="input-group mb-3">
           <input type="text"  placeholder="Email" name="email">
           
-        </div>
-        <div class="col-4">
+        
+        <div class="col-2">
           
           <button type="submit" name="submit_email" class="btn btn-primary btn-block">Register</button>
-        
+          </div>
         </div>
     </form>
   </body>
